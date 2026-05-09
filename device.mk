623cd428d893b8e051921bf6771c2214164fc719
r3nzph <abarracosorenznino@gmail.com>
2026-05-10 02:39:28 +0800

    Refactor device.mk for A/B OTA and package cleanup
    Removed redundant entries from PRODUCT_PACKAGES and reorganized the A/B OTA section.

--- device.mk
+++ device.mk
@@ -1,21 +1,35 @@
-ENABLE_VIRTUAL_AB := true
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+
+LOCAL_PATH := device/tecno/KJ5
+
+# ============================================
+# A/B OTA
+# ============================================
 AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
     boot \
     dtbo \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor \
+    vendor_boot \
     system \
     system_ext \
     product \
-    vendor \
-    vbmeta \
-    vendor_boot \
-    vbmeta_system \
-    vbmeta_vendor
+    vendor
 
-# A/B
+# ============================================
+# OTA / Update Engine
+# ============================================
 PRODUCT_PACKAGES += \
-    otapreopt_script
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -23,33 +37,23 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl \
-    update_engine_client
-
-# Bootctrl
+# ============================================
+# Boot Control HAL
+# ============================================
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.mt6768
 
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6768 \
+    libgptutils \
+    libz \
+    libcutils
+
+# ============================================
 # Fastbootd
+# ============================================
 PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
-
-# Keystore2
-PRODUCT_PACKAGES += \
-    android.system.keystore2
-
-# Update Engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-# Additional configs
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
-
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1
