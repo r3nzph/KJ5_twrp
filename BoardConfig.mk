a6d8d6824125ee1845d6b50d45da3c5ee44ea948
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-05-16 01:35:13 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,3 +184,7 @@ TW_CUSTOM_CPU_TEMP_PATH := sys/devices/virtual/thermal/thermal_zone4/temp
 
 # Device Version
 TW_DEVICE_VERSION := KJ5_By_Contributors
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_KJ5
+TARGET_RECOVERY_DEVICE_MODULES := libinit_KJ5
